eebc327dcf5f0cab1b671d7d5e0ff14613f566fc
dexter <bdextereri@yahoo.fr>
2020-02-02 14:18:09 +0100

    (feat): create routing for components and services

--- front/blogFront/src/app/app-routing.module.ts
+++ front/blogFront/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BoardUserComponent } from './components/board-user/board-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'user', component: BoardUserComponent },
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
